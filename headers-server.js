443597e232e3aa5bb356b3726a00945d001a0c2f
Gadi Cohen <dragon@wastelands.net>
2013-08-30 09:01:39 +0300

    fix regression introduced in 0.6.5 support

--- headers-server.js
+++ headers-server.js
@@ -7,15 +7,15 @@ headers = {
 
 var app = typeof WebApp != 'undefined' ? WebApp.connectHandlers : __meteor_bootstrap__.app;
 app.use(function(req, res, next) {
-    reqHeaders = req.headers;
+    headers.list = req.headers;
     return next();
 });
 
 Meteor.methods({
     'getReqHeader': function(header) {
-    	return reqHeaders[header];
+    	return headers.list[header];
     },
     'getReqHeaders': function() {
-    	return reqHeaders;
+    	return headers.list;
     }
 });
